baa5f071088ac6382941728d1dd65eb17660795d
ananttwr <ananttwr28@gmail.com>
2025-05-27 23:53:27 +0530

    Add implementation for Finding  the Majority Element that occurs more than N/2 times Moore’s Voting Algorithm

---
+++ Step 3 Arrays/medium/q3_find_the_majority_element_that_occurs_more_than_N_div_2_times.py
@@ -0,0 +1,31 @@
+'''
+Arrays - Medium
+Find the Majority Element that occurs more than N/2 times 27/5/25
+https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
+
+'''
+
+'''
+------------ Optimised -------------- TC - O(N), SC - O(1)
+
+Approach - Majority Element will not get cancelled out by rest as it has freq of > floor(N/2) Moore’s Voting Algorithm
+'''
+
+def find_majority_element(nums):
+    n = len(nums)
+    leader, c = 0, 0
+    for i in range(0, n):
+        if c == 0:
+            leader = nums[i]
+            c = 1
+        if nums[i] == leader:
+            c += 1
+        else:
+            c -= 1
+    return leader
+
+
+if __name__ == "__main__":
+    nums = [0,1,1,0,1,1,2,2]
+    majority_element = find_majority_element(nums)
+    print(f"The majority element is: {majority_element}")
