dd0f5dff08d7b49880b806372b5eb1811a13b587
ananttwr <ananttwr28@gmail.com>
2025-10-04 23:47:57 +0530

    Binary Tree Questions

--- Binary Tree Theory/binary_tree_theory.py
+++ Binary Tree Theory/binary_tree_theory.py
@@ -75,6 +75,88 @@ def levelorder(root):           # TC O(N) visiting each node at max once, SC O(M
 
 # <----------------------------------------------------------------------------------->
 		
+
+# <------------------------- Count of Nodes ------------------------------------------>
+
+def count_of_nodes(root):           # TC O(N), SC O(H)
+    if root is None:
+        return 0
+
+    left_count = count_of_nodes(root.left)    
+    right_count = count_of_nodes(root.right)    
+
+    return left_count + right_count + 1
+
+# <----------------------------------------------------------------------------------->
+
+
+# <------------------------- Sum of Nodes ------------------------------------------>
+
+def sum_of_nodes(root):           # TC O(N), SC O(H)
+    if root is None:
+        return 0
+
+    left_sum = sum_of_nodes(root.left)    
+    right_sum = sum_of_nodes(root.right)    
+
+    return left_sum + right_sum + root.val
+
+# <----------------------------------------------------------------------------------->
+
+# <------------------------- Height of Tree ------------------------------------------>
+
+def height_tree(root):           # TC O(N), SC O(H)
+    if root is None:
+        return 0
+
+    left_ht = height_tree(root.left)    
+    right_ht = height_tree(root.right)    
+
+    return max(left_ht, right_ht) + 1
+
+# <----------------------------------------------------------------------------------->
+
+# <------------------------- Diameter of Tree (Approach 1 TC O(N^2)) ------------------------------------------>
+
+def diameter(root):           # TC O(N^2), SC O(H+H) (At max.)
+    if root is None:
+        return 0
+    
+    diam_passing_node = height_tree(root.left) + height_tree(root.right) + 1        # ht of lt + rt + 1 for curr.
+    diam_left_sbtree = diameter(root.left)    
+    diam_right_sbtree = diameter(root.right)    
+    
+    return max(diam_passing_node, diam_left_sbtree, diam_right_sbtree)
+
+# <----------------------------------------------------------------------------------->
+
+
+# <------------------------- Diameter of Tree (Approach 2 TC O(N)) ------------------------------------------>
+
+class TreeInfo:
+    def __init__(self, height, diameter):
+        self.height = height
+        self.diameter = diameter
+
+def diameter2(root) -> TreeInfo:           # return type is TreeInfo class, as have to return two values height and diameter
+    if root is None:
+        return TreeInfo(0, 0)           # in base case return obj. of TreeInfo class with ht. and diam. as 0, 0
+    
+    left_tree_info = diameter2(root.left)       # This is DFS only, postorder as calculating curr node value after left and right
+    right_tree_info = diameter2(root.right)   
+    
+    my_height = max(left_tree_info.height, right_tree_info.height) + 1      # max of LH, RH + 1, we are calculating ht. and diam. in single function in 1 iteration only so O(N) TC
+
+    diam1 = left_tree_info.diameter     # LSTree Diam
+    diam2 = right_tree_info.diameter    # RSTree Diam
+    diam3 = left_tree_info.height + right_tree_info.height + 1
+
+    my_diam = max(diam1, diam2, diam3)
+    return TreeInfo(my_height, my_diam)
+
+# <----------------------------------------------------------------------------------->
+
+
 if __name__ == "__main__":			# 4. main fn. execution will start here, we will create binary tree here
     nodes = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1]
     # 5. create obj of BTree class
@@ -89,6 +171,26 @@ if __name__ == "__main__":			# 4. main fn. execution will start here, we will cr
 
     print()
     levelorder(root)
+
+# <------------------------- Count of Nodes ------------------------------------------>
+    num_of_nodes = count_of_nodes(root)
+    print(num_of_nodes)
+
+# <------------------------- Sum of Nodes ------------------------------------------>
+    summ = sum_of_nodes(root)
+    print(summ)
+
+# <------------------------- Height of Tree ------------------------------------------>
+    height = height_tree(root)
+    print(height)
+
+# <------------------------- Diameter of Tree ------------------------------------------>
+    diameter_tree = diameter(root)
+    print(diameter_tree)
+
+# <------------------------- Diameter of Tree O(N), SC O(H) ------------------------------------------>
+    diameter_tree2 = diameter2(root)
+    print(diameter_tree2.diameter)          # diameter2 will return obj. we have to access its attribute/ data
 
 
 """
